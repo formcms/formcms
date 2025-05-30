ff3b7d7461f94f33e4e196806b4320f984ce8404
fngch <fngch@hotmail.com>
2025-05-30 14:34:43 -0500

    committed video seperate demo app

---
+++ FormCMSVideoApp/Program.cs
@@ -0,0 +1,53 @@
+
+using FormCMS;
+using FormCMS.App;
+using FormCMS.Auth;
+using FormCMS.Auth.Builders;
+using FormCMS.Cms.Workers;
+using FormCMS.Infrastructure.EventStreaming;
+using FormCMS.Utils.ResultExt;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SqlVideoWebApp;
+
+var webBuilder = WebApplication.CreateBuilder(args);
+
+var connectionString = webBuilder.Configuration.GetConnectionString("sqlserver")!;
+var natsConnectionString =
+    webBuilder.Configuration.GetConnectionString("nats")
+    ?? throw new Exception("missing nats connection");
+webBuilder.Services.AddOutputCache();
+
+webBuilder.Services.AddSqlServerCms(connectionString);
+
+//add permission control service
+webBuilder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+webBuilder.Services.AddCmsAuth<IdentityUser, IdentityRole, AppDbContext>(new AuthConfig());
+webBuilder.Services.AddAuditLog();
+webBuilder.Services.AddActivity();
+webBuilder.Services.AddNatsMessageProducer(["asset"]);
+webBuilder.AddNatsClient(AppConstants.Nats);
+webBuilder.Services.AddSingleton<IStringMessageProducer, NatsProducer>();
+
+var webApp = webBuilder.Build();
+
+//ensure identity tables are created
+using var scope = webApp.Services.CreateScope();
+var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+await ctx.Database.EnsureCreatedAsync();
+
+//use cms' CRUD
+await webApp.UseCmsAsync();
+
+//add two default admin users
+await webApp.EnsureCmsUser("sadmin@cms.com", "Admin1!", [Roles.Sa]).Ok();
+await webApp.EnsureCmsUser("admin@cms.com", "Admin1!", [Roles.Admin]).Ok();
+
+//worker run in the background do Cron jobs
+var workerBuilder = Host.CreateApplicationBuilder(args);
+workerBuilder.AddNatsClient(AppConstants.Nats);
+workerBuilder.Services.AddSingleton<IStringMessageConsumer, NatsConsumer>();
+
+workerBuilder.Services.AddSqlServerCmsWorker(connectionString).WithNats(natsConnectionString);
+
+await Task.WhenAll(webApp.RunAsync(), workerBuilder.Build().RunAsync());
